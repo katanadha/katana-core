f2f4e4176b1371d1b377a3a4f8bd08407d77b977
kyncore <yen-ning@coremobile.co.jp>
2026-01-06 20:29:12 +0900

    open 1.1-dev

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -44,10 +44,10 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     /** release workers - in order to execute */
     $services->set(UpdateReplaceReleaseWorker::class);
     $services->set(SetCurrentMutualDependenciesReleaseWorker::class);
-    $services->set(AddTagToChangelogReleaseWorker::class);
     $services->set(TagVersionReleaseWorker::class);
     $services->set(PushTagReleaseWorker::class);
-    // $services->set(SetNextMutualDependenciesReleaseWorker::class);
+    $services->set(SetNextMutualDependenciesReleaseWorker::class);
+    $services->set(AddTagToChangelogReleaseWorker::class);
     $services->set(UpdateBranchAliasReleaseWorker::class);
     $services->set(PushNextDevReleaseWorker::class);
 
